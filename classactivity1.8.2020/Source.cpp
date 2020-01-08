aea674623a9dc3e605687d06eba4b63268aaef55
Wesley Cottrill <WCottrill@my.gcu.edu>
2020-01-08 15:01:36 -0700

    Add the second question

--- classactivity1.8.2020/Source.cpp
+++ classactivity1.8.2020/Source.cpp
@@ -3,13 +3,34 @@
 // In class exercise
 
 #include <iostream>
+#include <math.h>
 using namespace std;
-int main() {
-	float kilogram, pound;
-	cout << "enter pound";
-	cin >> pound;
-	kilogram = pound * 0.454;
-	cout << pound << "pound=" << kilogram << "Kg";
+//int main() {
+//	/*float kilogram, pound;
+//	cout << "enter pound";
+//	cin >> pound;
+//	kilogram = pound * 0.454;
+//	cout << pound << "pound=" << kilogram << "Kg";*/
+//
+//	
+//
+//	return 0;
+//}
+
+//double hexagonArea(double s)
+//{
+//	return ((3 * sqrt(3) *
+//		(s * s)) / 2);
+//}
+
+int main()
+{
+	float side, area;
+	cout << "enter side";
+	cin >> side;
+	area = ((3 * sqrt(3) *
+		(side * side)) / 2);
+	cout << side << "side=" << area << "area";
 
 	return 0;
 }
