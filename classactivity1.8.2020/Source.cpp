7036827ecfb3a7763032dadd74aa8856536e6cf7
Wesley Cottrill <WCottrill@my.gcu.edu>
2020-01-08 14:26:03 -0700

    Initial Commit

---
+++ classactivity1.8.2020/Source.cpp
@@ -0,0 +1,15 @@
+// Wesley Cottrill
+// This is my own work
+// In class exercise
+
+#include <iostream>
+using namespace std;
+int main() {
+	float kilogram, pound;
+	cout << "enter pound";
+	cin >> pound;
+	kilogram = pound * 0.454;
+	cout << pound << "pound=" << kilogram << "Kg";
+
+	return 0;
+}
