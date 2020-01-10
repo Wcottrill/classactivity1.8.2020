d07df2cd48ec977d1fee5f5da4bf1ea75f52cb7c
Wesley Cottrill <WCottrill@my.gcu.edu>
2020-01-10 14:35:58 -0700

    This is the 3rd and last question on the in class activity.

--- classactivity1.8.2020/Source.cpp
+++ classactivity1.8.2020/Source.cpp
@@ -3,7 +3,7 @@
 // In class exercise
 
 #include <iostream>
-#include <math.h>
+#include <conio.h>
 using namespace std;
 //int main() {
 //	/*float kilogram, pound;
@@ -23,14 +23,35 @@ using namespace std;
 //		(s * s)) / 2);
 //}
 
-int main()
-{
-	float side, area;
-	cout << "enter side";
-	cin >> side;
-	area = ((3 * sqrt(3) *
-		(side * side)) / 2);
-	cout << side << "side=" << area << "area";
+//int main()
+//{
+//	float side, area;
+//	cout << "enter side";
+//	cin >> side;
+//	area = ((3 * sqrt(3) *
+//		(side * side)) / 2);
+//	cout << side << "side=" << area << "area";
+//
+//	return 0;
+//}
+
+
+int main() {
+
+
+	int a, b, big;
+	cout << "Please enter 2 digits sir/madam: ";
+	cin >> a >> b;
+	if (a > b)
+	{
+		big = a;
+	}
+	else
+	{
+		big = b;
+	}
+	cout << "The biggest number of the two is:"<<big;
 
 	return 0;
+
 }
